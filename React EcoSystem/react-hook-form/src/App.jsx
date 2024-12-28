58db573908a59f72d605bd896187bf0ddfc1cd0f
morsalin-islam5350 <weshallovercome335@gmail.com>
2024-12-28 22:35:34 +0600

    react hook form

---
+++ React EcoSystem/react-hook-form/src/App.jsx
@@ -0,0 +1,14 @@
+import LoginForm from "./forms/LoginForm"
+import RegistrationForm from "./forms/RegistrationForm"
+
+function App() {
+
+  return (
+    <>
+      {/*<LoginForm />*/}
+      <RegistrationForm />
+    </>
+  )
+}
+
+export default App
