ba8576843861033c05a795ae2478a563da6d7bf3
morsalin-islam5350 <weshallovercome335@gmail.com>
2024-12-31 22:06:50 +0600

    dive into react uncommon hooks

---
+++ React EcoSystem/dive-into-react-uncommon-hooks/src/App.jsx
@@ -0,0 +1,8 @@
+import GitHubProfile from "./debug/GitHubProfile";
+
+function App() {
+  // return <LayoutTest />;
+  <GitHubProfile />;
+}
+
+export default App;
---
+++ React EcoSystem/dive-into-react-uncommon-hooks/src/debug/GitHubProfile.jsx
@@ -0,0 +1,50 @@
+import useGitHub from "./useGitHub";
+
+const GitHubProfile = () => {
+  const { user, loading, error } = useGitHub("morsalin-islam335");
+
+  //   if (loading)
+  //     return (
+  //       <>
+  //         <p>loading .....</p>
+  //       </>
+  //     );
+  //   if (error)
+  //     return (
+  //       <>
+  //         <p>error on fetching data .....</p>
+  //       </>
+  //     );
+  //   return (
+  //     <>
+  //       {user ? (
+  //         <div className="flex flex-col justify-center items-center p-4">
+  //           <img
+  //             src={user["avatar_url"]}
+  //             alt="photo"
+  //             className="border rounded-full w-32 h-32"
+  //           />
+  //           <p className="text-3xl my-3">
+  //             {user?.name} has <u>{user["public_repos"]}</u> public repositories
+  //             that gained <u>{user.followers}</u> followers.
+  //           </p>
+  //           <h2 className="text-xl">More About the contributor:</h2>
+  //           <p className="my-2 text-md bg-gray-300 border p-2 rounded-md">
+  //             {user?.bio}
+  //           </p>
+  //         </div>
+  //       ) : (
+  //         <p>No user data</p>
+  //       )}
+  //     </>
+  //   );
+  {
+    <>
+      <div>{user}</div>
+      <div>{loading}</div>
+      <div>{error}</div>
+    </>;
+  }
+};
+
+export default GitHubProfile;
