a154e6e274ebd0ef59e7e0fd40f65e46f7596acb
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-02-22 07:44:34 +0600

    add word omitter hackerrank hard tag react problem solution

---
+++ HackerrankProblem/WordOmitter/WordOmitter.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+const OMITTED_WORDS = ["a", "the", "and", "or", "but"];
+
+function WordOmitter() {
+  const [inputText, setInputText] = useState("");
+  const [omitWords, setOmitWords] = useState(true);
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const toggleOmitWords = () => {
+    setOmitWords(!omitWords);
+  };
+
+  const clearFields = () => {
+    // TODO: Add your changes here
+    setOmitWords(() => true);
+
+    setInputText(() => "");
+  };
+
+  const getProcessedText = () => {
+    // TODO: Add your changes here
+    if (omitWords) {
+      const inputWords = inputText.split(" ");
+      const wordsAfterOmit = inputWords.filter(
+        (item) => !OMITTED_WORDS.includes(item)
+      );
+      const omitedSentence = wordsAfterOmit.join(" ");
+      return omitedSentence;
+    }
+    return inputText;
+  };
+
+  return (
+    <div className="omitter-wrapper">
+      <textarea
+        placeholder="Type here..."
+        value={inputText}
+        onChange={handleInputChange}
+        data-testid="input-area"
+      />
+      <div>
+        <button onClick={toggleOmitWords} data-testid="action-btn">
+          {omitWords ? "Show All Words" : "Omit Words"}
+        </button>
+        <button onClick={clearFields} data-testid="clear-btn">
+          Clear
+        </button>
+      </div>
+      <div>
+        <h2>Output:</h2>
+        <p data-testid="output-text">{getProcessedText()}</p>
+      </div>
+    </div>
+  );
+}
+
+export { WordOmitter };
