c722e5358b69d1a1883d637f03fc757e91830282
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-02-22 07:56:25 +0600

    add ItemListManager hackerrank medium problem solution

---
+++ HackerrankProblem/ItemListManager/ItemListManager.jsx
@@ -0,0 +1,48 @@
+import "h8k-components";
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleAddItem = () => {
+    // TODO: Add logic to add input to items listm
+    if (input === "") {
+      return;
+    } else {
+      setItems(() => [...items, input]);
+    }
+
+    setInput(() => "");
+  };
+
+  return (
+    <>
+      <h8k-navbar header="Item List Manager"></h8k-navbar>
+      <div className="App">
+        <h3>Item List</h3>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter item"
+          data-testid="input-field"
+        />
+        <button onClick={handleAddItem} data-testid="add-button">
+          Add Item
+        </button>
+        <ul data-testid="item-list">
+          {items.map((item, index) => (
+            <li key={index} data-testid="list-item">
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
